bab57a0b2f13b0421d9fca7341a6502960c5d01c
Atif-1 <atif1712001@gmail.com>
2024-03-18 00:21:00 +0530

    optimised premium feature

--- controller/premium.js
+++ controller/premium.js
@@ -1,32 +1,21 @@
 const Expense=require('../model/expense');
 const User=require('../model/user');
+const sequelize = require('../util/database');
 
 exports.getLeaderboard=async(req,res,next)=>{
 	try{
-	const users=await  User.findAll();
-	const expenses=await Expense.findAll();
-	const totalExpenses={};
-	expenses.forEach((expense) => {
-		if(totalExpenses[expense.userId]){
-			totalExpenses[expense.userId]+=expense.amount;
-		}
-		else{
-			totalExpenses[expense.userId]=expense.amount;
-		}
-	});
-	console.log(totalExpenses);
-	const board=[];
-	users.forEach((user)=>{
-		if(totalExpenses[user.id]){
-			board.push({name:user.name,TotalExpense:totalExpenses[user.id]});
-		}
-		else{
-			board.push({name:user.name,TotalExpense:0});
-		}
-	});
-	board.sort((a,b)=>b.TotalExpense-a.TotalExpense);
-	console.log(board);
-	res.status(200).json(board);
+	const leaderboardOfUser=await User.findAll({
+		attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'total_cost']],
+		include:[
+			{
+				model:Expense,
+				attributes:[]
+			}
+		],
+		group:['user.id'],
+		order:[['total_cost','DESC']]
+	})
+	res.status(200).json(leaderboardOfUser);
 	}catch(err){
 		console.log(err);
 		res.status(500).json(err);
