92b0918d7f7e11595b45dacffdcb1d17d45a29ed
Atif-1 <atif1712001@gmail.com>
2024-03-20 01:16:29 +0530

    expense tracker app with transactions rollback feature

--- controller/expense.js
+++ controller/expense.js
@@ -1,7 +1,9 @@
 const Expense=require('../model/expense');
 const User=require('../model/user');
+const sequelize=require('../util/database');
 
 exports.addExpense=async(req,res,next)=>{
+	const t=await sequelize.transaction();
 	try{	
 		const amount=req.body.amount;
 		const description=req.body.description;
@@ -9,10 +11,14 @@ exports.addExpense=async(req,res,next)=>{
 		const user=await User.findByPk(req.user.id);
 		const currentTotal=parseInt(await user.totalexpenses);
 		const recentAmt=parseInt(amount);
-		await Expense.create({amount:amount,description:description,category:category,userId:req.user.id});
-		await User.update({totalexpenses:currentTotal+recentAmt},{where:{id:req.user.id}});
+		await Expense.create({amount:amount,description:description,category:category,userId:req.user.id},{transaction:t});
+		await User.update({totalexpenses:currentTotal+recentAmt},{where:{id:req.user.id},transaction:t});
+		await t.commit();
 		res.status(200).json({success:true,message:"Expense added successfully"});
-	}catch(err){res.json({success:false,message:err});}
+	}catch(err){
+		await t.rollback();
+		res.json({success:false,message:err});
+	}
 }
 
 exports.getExpenses=async (req,res,next)=>{
@@ -27,12 +33,21 @@ exports.getExpenses=async (req,res,next)=>{
 }
 
 exports.deleteExpense=async (req,res,next)=>{
-	try{
+	const t=await sequelize.transaction();
+	try{ 
 	const id=req.params.id;
-	await Expense.destroy({where:{id:id,userId:req.user.id}});
+	const user=await User.findByPk(req.user.id);
+	const deletedExpense=await Expense.findOne({where:
+	{id:id}});
+	const newTotalExpenses=Number(user.totalexpenses)-Number(deletedExpense.amount);
+	
+	await Expense.destroy({where:{id:id,userId:req.user.id},transaction:t});
+	await User.update({totalexpenses:newTotalExpenses},{where:{id:req.user.id},transaction:t});
+	await t.commit();
 	res.json({success:true,message:"deleted successfully"});
 	}
 	catch(err){
+		await t.rollback();
 		console.log(err);
 	}
 }
