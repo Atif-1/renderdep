b7188d9a6ee2d09c7bbe822b50310d397581ba4e
Atif-1 <atif1712001@gmail.com>
2024-06-30 17:30:40 +0530

    .

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize=require('sequelize');
 require("dotenv").config();
-const sequelize=new Sequelize(process.env.TABLE_NAME,process.env.DATABASE_TYPE,process.env.DATABASE_PASSWORD,{
+const sequelize=new Sequelize(process.env.TABLE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASSWORD,{
 	dialect:'mysql',host:process.env.HOST
 });
 
